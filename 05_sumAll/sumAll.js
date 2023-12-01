70532feef88eaf85f87b39e62410bbe48b8695e5
LincolnHubbard <hubbard.lincoln@gmail.com>
2023-11-30 18:12:50 -0800

    Solve exercise 5 and pass unit test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    
+    if (start < 0 || end < 0 || !Number.isInteger(start) || 
+        !Number.isInteger(end)){
+        return "ERROR";
+    }
+    let count = 0;
+    if (start > end){
+        for (let i = start; i >= end; i--){
+            count += i;
+        }
+    }
+    else{
+        for (let i = start; i <= end; i++){
+            count += i;
+        }
+    }
+    return count;
 };
 
 // Do not edit below this line
