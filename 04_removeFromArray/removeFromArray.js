6468031fd1ca03248b11d639d9f46d275e8fd17d
LincolnHubbard <hubbard.lincoln@gmail.com>
2023-11-30 17:24:21 -0800

    Solve exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(input, ...args) {
+    const output = [];
 
+    input.forEach((index) => {
+        if (!args.includes(index)){
+            output.push(index);
+        }
+    })
+
+    return output;
 };
 
 // Do not edit below this line
