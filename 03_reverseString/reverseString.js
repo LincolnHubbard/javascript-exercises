e9cfb300cbda1d7f440d1fbd7c0a9034f8e4705f
LincolnHubbard <hubbard.lincoln@gmail.com>
2023-11-30 16:50:42 -0800

    Solve exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(input) {
 
+    let charArray = input.split('');
+    charArray = charArray.reverse();
+    let output = charArray.join('');
+
+    // for (let i = 1; i < input.length; i++){
+    //     // charBuffer = input.slice(-i);
+    //     output += charArray[-i];
+    // }
+
+    return output;
 };
 
 // Do not edit below this line
