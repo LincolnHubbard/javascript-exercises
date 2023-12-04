a36aadce3d3148e17bbb00ba5e1b100661c3c00e
LincolnHubbard <hubbard.lincoln@gmail.com>
2023-12-04 13:37:10 -0800

    Solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0){
+        return "OOPS";
+    }
+    if (n === 0){
+        return 0;
+    }
 
+    let num1 = 1;
+    let num2 = 0;
+    let index = 2;
+
+    while (index <= n){
+        let current = num1 + num2;
+        num2 = num1;
+        num1 = current;
+        index++;
+    }
+
+    return num1;
 };
 
 // Do not edit below this line
