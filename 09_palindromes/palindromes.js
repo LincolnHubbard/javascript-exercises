8d42fcc60c142f24ee8549b21225dcdad8881869
LincolnHubbard <hubbard.lincoln@gmail.com>
2023-12-04 13:28:35 -0800

    Solve exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+   // let joined = input.join('');
+   let cleanString = input.toLowerCase().replace(/[^a-z0-9]/g, "");
+   let reversedString = cleanString.split("").reverse().join("");
+   console.log(reversedString);
+    if (cleanString === reversedString){
+        return true;
+    }
+    else{
+        return false;
+    }
 };
 
 // Do not edit below this line
