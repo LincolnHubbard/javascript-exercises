99043454c87429d90e95cb720ad7a047d8461904
LincolnHubbard <hubbard.lincoln@gmail.com>
2023-12-04 03:29:53 -0800

    Solve exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,35 @@
-const add = function() {
+const add = function(input1, input2) {
+	return input1 + input2;
+};
+
+const subtract = function(input1, input2) {
+  return input1 - input2;
+};
+
+const sum = function(nums) {
+  return nums.reduce((total, currentValue) => total + currentValue, 0)
+};
+
+const multiply = function(nums) {
+  return nums.reduce((total, currentValue) => total * currentValue)
+};
+
+const power = function(input1, input2) {
+  return Math.pow(input1, input2)
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(input) {
+  let result = 1;
+  if (input === 0 || input === 1){
+    return 1;
+  }
+  else{
+    for (let i = input; i > 0; i--){
+      result = result * i;
+    }
+    return result;
+  }
 	
 };
 
