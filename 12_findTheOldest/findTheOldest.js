c831594fa19756af96e6ebbadf224ceebb21c07c
LincolnHubbard <hubbard.lincoln@gmail.com>
2023-12-04 16:33:08 -0800

    Solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldestPerson, currentPerson) =>{
+        const oldestAge = calculateAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+        const currentAge = calculateAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        if (currentAge > oldestAge){
+            return currentPerson;
+        }
+        else{
+            return oldestPerson;
+        }
+    });
+};
+
+
+function calculateAge(birthYear, deathYear){
+    if(!deathYear){
+        deathYear = new Date().getFullYear();
+    }
+
+    let age = deathYear - birthYear;
+    return age;
 
 };
 
